37fb5391c3d1845cffeb7440207471e710c12d31
adittrp <166170305+adittrp@users.noreply.github.com>
2026-07-18 00:02:50 -0500

    Add project files.

---
+++ MinecraftModLauncher/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MinecraftModLauncher.ViewModels {
+    public abstract class ViewModelBase : ObservableObject {
+    }
+}
---
+++ MinecraftModLauncher/Views/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace MinecraftModLauncher.Views {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
